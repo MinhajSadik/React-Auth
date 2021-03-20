bc3e435fb60d211d4dcf32f54d173438d1544efd
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-20 21:21:16 +0600

    working done

--- src/components/TransportCard/TransportCard.js
+++ src/components/TransportCard/TransportCard.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import './TransportCard.css';
 
 const TransportCard = (props) => {
-    const { strID, name, icon } = props.transport;
+    const { strID, name, icon, fare } = props.transport;
     return (
         <Link className="transport__card" to={`/destination/${strID}`}>
             <div className="transport__img--box">
                 <img src={icon} alt={name} className="transport__img" />
             </div>
             <h2 className="transport__name">{name}</h2>
+            <h3>This is Fare: {fare}</h3>
         </Link>
     );
 };
